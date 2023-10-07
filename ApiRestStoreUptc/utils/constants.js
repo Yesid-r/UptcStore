1f2634396c052f908ee3cff76544c8fbd3eb43fc
Yesid Rincon <dairo.rincon@uptc.edu.co>
2023-10-07 13:44:02 -0500

    add url frontend

--- ApiRestStoreUptc/utils/constants.js
+++ ApiRestStoreUptc/utils/constants.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://uptc-store-efl0z6ol0-yesid-r.vercel.app";
-export const FRONT_URL = "http://localhost:5173";
+export const FRONT_URL = "https://uptc-store-5vra.vercel.app";
