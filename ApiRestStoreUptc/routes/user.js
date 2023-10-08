909facfc621bc709fb603dac0d76df1fce635174
SebastianL2 <sebas16cely@gmail.com>
2023-10-07 23:01:09 -0500

    "no quitar"

--- ApiRestStoreUptc/routes/user.js
+++ ApiRestStoreUptc/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {obtainAllUsers, saveUser, findUserById, deleteUser, loginUser, updatePassword, sendMailRecoveryPass, updateUser} from '../controllers/controll_user.js'
+import {obtainAllUsers, saveUser, findUserById, deleteUser, loginUser, updatePassword, sendMailRecoveryPass, updateUser,loginAdminSeller} from '../controllers/controll_user.js'
 const router = express.Router()
 
 
@@ -9,7 +9,7 @@ router.post('/', saveUser)
 router.get('/:id', findUserById)
 router.delete('/:id', deleteUser)
 router.post('/login', loginUser)  
-// router.post('/login2', loginAdminSeller)  
+router.post('/login2', loginAdminSeller)  
 router.post('/updatePassword', updatePassword)  
 router.post('/sendEmail/:email', sendMailRecoveryPass)
 router.put('/:id', updateUser)
