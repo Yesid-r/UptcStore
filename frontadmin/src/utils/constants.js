1952f42fd1142efd9a65a6ce6e4429e08aaba76a
SebastianL2 <sebas16cely@gmail.com>
2023-10-23 23:09:22 -0500

    23

--- frontadmin/src/utils/constants.js
+++ frontadmin/src/utils/constants.js
@@ -1,2 +1,2 @@
-//export const API_URL = "https://uptc-store.vercel.app"
-export const API_URL = "http://localhost:3001"
+export const API_URL = "https://uptc-store.vercel.app"
+//export const API_URL = "http://localhost:3001"
